4e64bfcdcfb5f25aa22b3696051ee22c8f7be363
Sos7ya <k.moskal9901@gmail.com>
2022-11-22 19:29:13 +0300

    added navbar

--- components/Main/Header/Navigation/index.tsx
+++ components/Main/Header/Navigation/index.tsx
@@ -1,6 +1,14 @@
+import cx from './index.module.scss';
+
 const MainPageNavigation = ()=>{
-    return <div>
-        MainPageNavigation
+    return <div className={cx.navbar}>
+        <ul className={cx.navbar__list}>
+            <li><h3>Новинки</h3></li>
+            <li><h3>Акции</h3></li>
+            <li><h3>PageName</h3></li>
+            <li><h3>PageName</h3></li>
+            <li><h3>PageName</h3></li>
+        </ul>
     </div>
 }
 

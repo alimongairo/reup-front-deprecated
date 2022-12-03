b4cbd59d1d0e7964848acdc9d558941247aa5674
arifullinrd <arifullinrd@lad24.ru>
2022-12-03 18:33:33 +0600

    слайдер

--- components/Main/Slider/index.tsx
+++ components/Main/Slider/index.tsx
@@ -1,38 +1,80 @@
-import { useEffect, useRef, useState } from 'react';
-import BlockOne from './BlockOne';
-import BlockTwo from './BlockTwo';
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
 import cx from './index.module.scss';
 
-const Slider = () => {
-  const [oneSlide, setOneSlide] = useState(true);
+import leftArrow from '@/static/icons/leftArrow.svg';
+import slide1 from '@/static/img/slide1.png';
+import slide2 from '@/static/img/SliderOne_two.png';
 
-  const clickArrow = () => {
-    setOneSlide(!oneSlide);
-  }
+import Slide, { ISlide } from '@/components/Main/Slider/Slide';
+
+const slideCount = 1;
+
+const slideArray: ISlide[] = [
+  {
+    id: 0,
+    active: false,
+    brandDescription:
+      'Наше дело не так однозначно, как может показаться: существующая теория напрямую зависит от стандартных подходов. Идейные соображения высшего порядка, а также разбавленное изрядной долей',
+    smallImg: slide2,
+    bigImg: slide1,
+    brandName: 'Имя бренда 1',
+  },
+  {
+    id: 1,
+    active: false,
+    brandDescription:
+      'Наше дело не так однозначно, как может показаться: существующая теория напрямую зависит от стандартных подходов. Идейные соображения высшего порядка, а также разбавленное изрядной долей',
+    smallImg: slide2,
+    bigImg: slide1,
+    brandName: 'Имя бренда 2',
+  },
+];
+
+const Slider = () => {
+  const [slideNumber, setSlideNumber] = useState(0);
+
+  const onNextSlide = () => {
+    setSlideNumber((state) => {
+      if (state + 1 > slideCount) {
+        return 0;
+      }
+      return state + 1;
+    });
+  };
+  const onPrevSlide = () => {
+    setSlideNumber((state) => {
+      if (state - 1 < 0) {
+        return slideCount;
+      }
+      return state - 1;
+    });
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setOneSlide(!oneSlide);
+      onNextSlide();
     }, 5000);
     return () => {
       clearInterval(interval);
     };
-  }, [oneSlide]);
+  }, []);
 
   return (
     <div className={cx.wrapper}>
-      
-      <div onClick={() => clickArrow()} className={cx.arrowLeft}>
-        <img src="../../../static/Slider/icons/Arrow-left.svg" alt="Arrow left" />
+      <div onClick={onPrevSlide} className={cx.arrowLeft}>
+        <Image src={leftArrow} alt="Arrow left" />
       </div>
-
-      {oneSlide ? <BlockOne /> : <BlockTwo />}
-
-      <div onClick={() => clickArrow()} className={cx.arrowRight}>
-        <img src="../../../static/Slider/icons/Arrow-right.svg" alt="Arrow right" />
-      </div>    
+      {slideArray.map((slide) => {
+        return (
+          <Slide key={slide.id} {...slide} active={slide.id === slideNumber} />
+        );
+      })}
+      <div onClick={onNextSlide} className={cx.arrowRight}>
+        <Image src={leftArrow} alt="Arrow left" />
+      </div>
     </div>
-  ) 
+  );
 };
 
 export default Slider;

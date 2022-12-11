2cb7575d8034c54497c3cd80a2c23368bf29c95e
arifullinrd <arifullinrd@lad24.ru>
2022-12-11 06:32:51 +0600

    правки билда

--- components/Main/ProductList/index.tsx
+++ components/Main/ProductList/index.tsx
@@ -25,6 +25,7 @@ const ProductsList = ({ productList }: IProps) => {
   const cardList = [...productList, ...productList].map((product) => {
     return (
       <ProductCard
+        key={product.id}
         onLike={onLikeHandler}
         onAddToBasket={onAddToBasketHandler}
         title={product.title}

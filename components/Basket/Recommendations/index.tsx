b74df0f5edea4d3e2122ddf25c4a55a69122d47d
rrrlong <danielsavatin@gmail.com>
2022-12-19 16:19:21 +0300

    replace headings

--- components/Basket/Recommendations/index.tsx
+++ components/Basket/Recommendations/index.tsx
@@ -5,6 +5,7 @@ import ProductsList from '@/components/Main/ProductList';
 import { getBasketLoading } from '@/store/basket/selectors';
 import { getProductListDataSource } from '@/store/productList/selectors';
 import { useAppSelector } from '@/hooks/store';
+import Heading from '@/components/common/Heading';
 
 const Recommendations = () => {
   const products = useAppSelector(getProductListDataSource);
@@ -16,9 +17,9 @@ const Recommendations = () => {
 
   return (
     <>
-      <h1>подобрали для тебя </h1>
+      <Heading>подобрали для тебя </Heading>
       <ProductsList productList={products} />
-      <h1>избранное</h1>
+      <Heading>избранное</Heading>
       <ProductsList productList={products} />
     </>
   );

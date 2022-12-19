5f73cdb6673f2c784c35e9aa337cea3076eada48
rrrlong <danielsavatin@gmail.com>
2022-12-19 16:23:19 +0300

    build fix

--- components/Basket/BasketItems/BasketSummary/index.tsx
+++ components/Basket/BasketItems/BasketSummary/index.tsx
@@ -11,41 +11,39 @@ interface IProps {
 
 const BasketSummary = ({ truePrice, oldPpice, count }: IProps) => {
   return (
-    <ContaineredAffix
-      children={
-        <div className={cx.wrapper}>
-          <div className={cx.total}>
-            <div>итого:</div>
-            <div>{truePrice} ₽</div>
-          </div>
-
-          <div className={cx.priceDetails}>
-            <div>
-              <div>товары {count} шт:</div>
-              <div>{oldPpice} ₽</div>
-            </div>
-            <div>
-              <div>скидка:</div>
-              <div>-{oldPpice - truePrice} ₽</div>
-            </div>
-            <div>
-              <div>доставка:</div>
-              <div>бесплатно</div>
-            </div>
-          </div>
-
-          <input
-            type="text"
-            className={cx.promo}
-            placeholder="введите промокод"
-          ></input>
-
-          <Link href={`${EPagesRoutes.Order}`}>
-            <button className={cx.checkoutBtn}>оформить заказ</button>
-          </Link>
+    <ContaineredAffix>
+      <div className={cx.wrapper}>
+        <div className={cx.total}>
+          <div>итого:</div>
+          <div>{truePrice} ₽</div>
         </div>
-      }
-    />
+
+        <div className={cx.priceDetails}>
+          <div>
+            <div>товары {count} шт:</div>
+            <div>{oldPpice} ₽</div>
+          </div>
+          <div>
+            <div>скидка:</div>
+            <div>-{oldPpice - truePrice} ₽</div>
+          </div>
+          <div>
+            <div>доставка:</div>
+            <div>бесплатно</div>
+          </div>
+        </div>
+
+        <input
+          type="text"
+          className={cx.promo}
+          placeholder="введите промокод"
+        ></input>
+
+        <Link href={`${EPagesRoutes.Order}`}>
+          <button className={cx.checkoutBtn}>оформить заказ</button>
+        </Link>
+      </div>
+    </ContaineredAffix>
   );
 };
 

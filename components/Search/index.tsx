145737cdb24a1d35f161d603f7f40204b2c9d8ee
Roman <krivosheev1605@gmail.com>
2023-01-27 14:56:32 +0300

    ad

--- components/Search/index.tsx
+++ components/Search/index.tsx
@@ -3,12 +3,9 @@ import searchLogo from '@/static/icons/serch.svg';
 import cx from './index.module.scss';
 
 const Search = () => {
-
-
-
   return (
     <div className={cx.search__panel}>
-      <input className={cx.search__panel__input} type='search'/>
+      <input className={cx.search__panel__input} type="search" />
       <Image src={searchLogo} alt="searchLogo"></Image>
     </div>
   );

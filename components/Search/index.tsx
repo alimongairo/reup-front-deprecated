1ca5c995e10be6b894a510da8d139d2701dfb208
Roman <krivosheev1605@gmail.com>
2023-01-30 15:26:22 +0300

    added changes

--- components/Search/index.tsx
+++ components/Search/index.tsx
@@ -1,10 +1,15 @@
-import { createContext, useCallback, useContext, useRef, useState } from 'react';
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useRef,
+  useState,
+} from 'react';
 import Image from 'next/image';
 import searchLogo from '@/static/icons/serch.svg';
 import cx from './index.module.scss';
 
 const Search = () => {
-
   const [value, setValue] = useState('');
   // const { searchValue, setSearchValue } = useState('');
   const inputRef = useRef();
@@ -12,11 +17,11 @@ const Search = () => {
   // const onClickClear = () => {
   //   setValue('');
   //   setSearchValue('');
-  //   inputRef.current.focus(); 
+  //   inputRef.current.focus();
   // };
 
   // const updateSearchValue = useCallback (
-  //   setTimeout((str) => { 
+  //   setTimeout((str) => {
   //     setSearchValue(str);
   //   }, 300),
   //   [],
@@ -25,13 +30,14 @@ const Search = () => {
   const onChangeInput = (e) => {
     setValue(e.target.value);
     // updateSearchValue(e.target.value);
-  }
+  };
 
   return (
     <div className={cx.search__panel}>
-      <input className={cx.search__panel__input}  
+      <input
+        className={cx.search__panel__input}
         onChange={onChangeInput}
-        value={value} 
+        value={value}
       />
       <Image src={searchLogo} alt="searchLogo"></Image>
     </div>

7ee3ac59bd09ebfebe71409926dddfc5b92e57de
Roman <krivosheev1605@gmail.com>
2023-01-31 15:48:59 +0300

    add

---
+++ components/Search/ModalSearch/index.tsx
@@ -0,0 +1,12 @@
+import cx from './index.module.scss';
+
+const ModalSearch = () => {
+
+  return (
+    <div className={cx.search__panel}>
+      <p>modal</p>
+    </div>
+  );
+};
+
+export default ModalSearch;
--- components/Search/index.tsx
+++ components/Search/index.tsx
@@ -1,44 +1,14 @@
-import {
-  createContext,
-  useCallback,
-  useContext,
-  useRef,
-  useState,
-} from 'react';
 import Image from 'next/image';
 import searchLogo from '@/static/icons/serch.svg';
 import cx from './index.module.scss';
+import { useState } from 'react';
 
 const Search = () => {
-  const [value, setValue] = useState('');
-  // const { searchValue, setSearchValue } = useState('');
-  const inputRef = useRef();
-
-  // const onClickClear = () => {
-  //   setValue('');
-  //   setSearchValue('');
-  //   inputRef.current.focus();
-  // };
-
-  // const updateSearchValue = useCallback (
-  //   setTimeout((str) => {
-  //     setSearchValue(str);
-  //   }, 300),
-  //   [],
-  // );
-
-  const onChangeInput = (e) => {
-    setValue(e.target.value);
-    // updateSearchValue(e.target.value);
-  };
+  
+  const [modalSearch, setModalSearch] = useState(false);
 
   return (
     <div className={cx.search__panel}>
-      <input
-        className={cx.search__panel__input}
-        onChange={onChangeInput}
-        value={value}
-      />
       <Image src={searchLogo} alt="searchLogo"></Image>
     </div>
   );

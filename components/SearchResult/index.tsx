c36d8071d114cf3fc7f054725f02dbbc8690dfaa
Roman <fhdha@yandex.ru>
2023-02-23 10:02:45 +0300

    add first

--- components/SearchResult/index.tsx
+++ components/SearchResult/index.tsx
@@ -1,14 +1,15 @@
 import cx from './index.module.scss';
 
 interface IProps {
-    searchValue: String;
-  }
+  searchValue: String;
+}
 
-const SearchResult = ({searchValue}:IProps) => {
-    return (
+const SearchResult = ({ searchValue }: IProps) => {
+  return (
     <div className={cx.searchResult}>
-        <h1>asdasdasd</h1>
+      <h1>asdasdasd</h1>
     </div>
-)}
+  );
+};
 
-export default SearchResult
+export default SearchResult;

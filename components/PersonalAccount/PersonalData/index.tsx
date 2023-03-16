2993bc25ca5ee09a1b5e5c2670b3f67d67fefc51
Roman <fhdha@yandex.ru>
2023-03-16 22:35:19 +0300

    add logic

--- components/PersonalAccount/PersonalData/index.tsx
+++ components/PersonalAccount/PersonalData/index.tsx
@@ -1,35 +1,32 @@
-import Button from '@/components/common/Button';
 import Heading from '@/components/common/Heading';
 import Input from '@/components/common/Input';
-import { useState } from 'react';
+import { useRef } from 'react';
 import cx from './index.module.scss';
 
 const PersonalData = () => {
-  const [name, setName] = useState();
+  const formDataObject: any = {};
 
-  const getFormValue = (event: any) => {
-    event.preventDefault();
-    console.log(event.target.name.value);
-  };
+  const formRef = useRef<any>();
 
-  const onChangeName = (event: any) => {
-    setName(event.target.value);
-    console.log(name);
+  const onChangeForm = (e: any) => {
+    e.preventDefault();
+
+    if (formRef.current) {
+      const formData = new FormData(formRef.current);
+      for (const [q, value] of formData) {
+        formDataObject[q] = value;
+        console.log(formDataObject);
+      }
+    }
   };
 
   return (
     <div className={cx.wrapper_presonal_data}>
-      <form id="form">
+      <form ref={formRef} onChange={onChangeForm} id="form">
         <div className={cx.flex_form}>
           <div className={cx.leftBlockform}>
             <Heading tag="h4">личные данные</Heading>
-            <Input
-              onChange={onChangeName}
-              type="text"
-              name="name"
-              placeholder="Иванов"
-              value={name}
-            />
+            <Input type="text" name="name" placeholder="Иванов" />
             <Input type="text" name="firstName" placeholder="Иван" />
             <Input type="text" name="lastName" placeholder="Иванович" />
             <Input type="date" name="age" placeholder="дд.мм.гггг" />
@@ -39,12 +36,12 @@ const PersonalData = () => {
             </Heading>
             <div className={cx.floor_checked}>
               <label>
-                <Input type="checkbox" name="floor" />
+                <Input type="checkbox" name="floorMen" />
                 мужской
               </label>
 
               <label className={cx.twoLabel}>
-                <Input type="checkbox" name="floor" />
+                <Input type="checkbox" name="floorWoomen" />
                 женский
               </label>
             </div>
@@ -52,20 +49,11 @@ const PersonalData = () => {
 
           <div className={cx.rightBlockform}>
             <Heading tag="h4">контактные данные</Heading>
-            <Input type="email" name="name" placeholder="электронная почта" />
-            <Input type="tel" name="name" placeholder="+7(999)999-99-99" />
-            <Input type="text" name="name" placeholder="Нижний Новгород" />
+            <Input type="email" name="mail" placeholder="электронная почта" />
+            <Input type="tel" name="tel" placeholder="+7(999)999-99-99" />
+            <Input type="text" name="city" placeholder="Нижний Новгород" />
           </div>
         </div>
-
-        {/* <Button className={cx.submit} onClick={(e) => getFormValue(e)}>сохранить</Button> */}
-        <button
-          type="submit"
-          className={cx.submit}
-          onClick={(e) => getFormValue(e)}
-        >
-          сохранить
-        </button>
       </form>
     </div>
   );

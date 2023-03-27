af273cf0c1aa3212b4b70aff7e48e1506c8c4289
Roman <fhdha@yandex.ru>
2023-03-27 17:28:48 +0300

    add

--- components/SearchResult/SearchFilter/FilterStructure/index.tsx
+++ components/SearchResult/SearchFilter/FilterStructure/index.tsx
@@ -1,3 +1,5 @@
+import Checkbox from '@/components/common/Checkbox';
+import Collapse from '@/components/common/Collapse';
 import Text from '@/components/common/Text';
 import Minus from '@/static/icons/minuse.svg';
 import Plus from '@/static/icons/plus.svg';
@@ -15,20 +17,37 @@ const FilterStructure = () => {
   return (
     <div className={cx.wrapper_pattern}>
       <div className={cx.header_pattern}>
-        <Text>состав</Text>
-        {listActive ? (
-          <Image
-            className={cx.minus}
-            onClick={onClickListActive}
-            src={Minus}
-            alt="Minus"
-          />
-        ) : (
-          <Image onClick={onClickListActive} src={Plus} alt="Plus" />
-        )}
+        <Collapse
+          title="состав"
+          content={
+            <div>
+              <div className={cx.len}>
+                <input
+                  type="checkbox"
+                  className={cx.custom_checkbox}
+                  id="len"
+                  name="len"
+                  value="yes"
+                />
+                <label htmlFor="len">хлопок</label>
+              </div>
+
+              <div className={cx.klen}>
+                <input
+                  type="checkbox"
+                  className={cx.custom_checkbox}
+                  id="klen"
+                  name="klen"
+                  value="yes"
+                />
+                <label htmlFor="klen">синтетика</label>
+              </div>
+            </div>
+          }
+        />
       </div>
 
-      <div className={listActive ? cx.list_pattern : cx.list_pattern_dis}>
+      {/* <div className={listActive ? cx.list_pattern : cx.list_pattern_dis}>
         <div className={cx.len}>
           <input
             type="checkbox"
@@ -50,7 +69,7 @@ const FilterStructure = () => {
           />
           <label htmlFor="klen">синтетика</label>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };

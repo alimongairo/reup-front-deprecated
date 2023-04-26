285f2cda3b863ebcec48c8babe3a3df963c94419
Roman <krivosheev1605@gmail.com>
2023-04-26 15:17:40 +0300

    error form

--- components/Ordering/DataOrder/PersonalData/index.tsx
+++ components/Ordering/DataOrder/PersonalData/index.tsx
@@ -1,11 +1,34 @@
+import Checkbox from '@/components/common/Checkbox';
 import Heading from '@/components/common/Heading';
 import Input from '@/components/common/Input';
-import Checkbox from '@/components/common/Checkbox';
-import cx from './index.module.scss';
-import { IPropsOrder } from '../..';
 import Text from '@/components/common/Text';
+import { useEffect, useRef, useState } from 'react';
+import { IPropsOrder } from '../..';
+import cx from './index.module.scss';
 
 const PersonalData = ({ isLogined }: IPropsOrder) => {
+  const formDataObject: any = {};
+  const formRef = useRef<any>();
+  // const [formData, setFormData] = useState({});
+
+  const onChangeForm = () => {
+    if (formRef.current) {
+      const formData = new FormData(formRef.current);
+      for (const [q, value] of formData) {
+        formDataObject[q] = value;
+      }
+    }
+  };
+
+  useEffect(() => {
+    const formData = new FormData(formRef.current);
+    for (const [q, value] of formData) {
+      formDataObject[q] = value;
+    }
+  }, [formData]);
+
+  console.log(formDataObject);
+
   return (
     <div className={cx.wrapperPersonalData}>
       <Heading tag="h2">2. данные получателя</Heading>
@@ -26,21 +49,18 @@ const PersonalData = ({ isLogined }: IPropsOrder) => {
       </div>
 
       <div className={cx.checkbox}>
-        <div>
-          <Checkbox label="" labelPlacement="right" id={'checkAll'} />
-        </div>
-        <div>
-          <Text size="thin">
-            я ознакомился и согласен с политикой обработки персональных данных и
-            пользовательским соглашением
-          </Text>
-        </div>
-
-        {/* <Checkbox
-          label="я согласен получать новости об акциях и специальных предложениях"
-          labelPlacement="right"
-          id={'checkAll'}
-        /> */}
+        <form ref={formRef} onChange={onChangeForm} id="form">
+          <div>
+            <Checkbox label="" labelPlacement="right" id={'checkAll'} />
+          </div>
+          <label htmlFor="checkAll">
+            <Text size="thin">
+              я ознакомился и согласен с{' '}
+              <span>политикой обработки персональных данных</span> и
+              пользовательским соглашением
+            </Text>
+          </label>
+        </form>
       </div>
     </div>
   );

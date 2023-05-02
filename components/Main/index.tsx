0e606d1d52da32f969fc8127d828119cde7d53d0
Konstantin <k.moskal9901@gmail.com>
2023-05-02 21:26:12 +0300

    main fixes

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -9,12 +9,19 @@ import Category from '@/components/Main/Category';
 import Collections from '@/components/Main/Collections';
 import Heading from '@/components/common/Heading';
 import ReupChoosen from '@/components/Main/ReupChoosen';
+import Image from 'next/image';
 
 import { useAppDispatch, useAppSelector } from '@/hooks/store';
 import { getProductListDataSource } from '@/store/productList/selectors';
 import { getProductListAction } from '@/store/productList/thunk';
 
 import cx from './index.module.scss';
+
+import upcycle1 from '@/static/img/upcycle1.svg';
+import upcycle2 from '@/static/img/upcycle2.svg';
+import upcycle3 from '@/static/img/upcycle3.svg';
+import upcycle4 from '@/static/img/upcycle4.svg';
+import upcycle5 from '@/static/img/upcycle5.svg';
 
 const MainPageComponents = () => {
   const dispatch = useAppDispatch();
@@ -46,7 +53,11 @@ const MainPageComponents = () => {
       </div>
       <Marquee gradient={false} speed={60}>
         <span className={cx.marquee}>
-          upcycle upcycle upcycle upcycle upcycle upcycle
+          <Image src={upcycle1} alt="upcycle" className={cx.el}></Image>
+          <Image src={upcycle2} alt="upcycle" className={cx.el}></Image>
+          <Image src={upcycle3} alt="upcycle" className={cx.el}></Image>
+          <Image src={upcycle4} alt="upcycle" className={cx.el}></Image>
+          <Image src={upcycle5} alt="upcycle" className={cx.el}></Image>
         </span>
       </Marquee>
       <ProductsList productList={newProducts} />

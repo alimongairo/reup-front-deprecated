5f5522bc6bf0fc378abdeddf76727ccf22dcb825
Roman <fhdha@yandex.ru>
2023-05-20 23:37:50 +0300

    add zalupa konya

--- components/Main/Category/index.tsx
+++ components/Main/Category/index.tsx
@@ -5,14 +5,14 @@ import cx from './index.module.scss';
 const Category = () => {
   return (
     <div className={cx.wrapper}>
-      <Button className={cx.right}>локальные бренды</Button>
-      <Button className={cx.right}>парфюм</Button>
-      <Button className={cx.left}>обувь</Button>
-      <Button className={cx.left}>винтаж</Button>
-      <Button className={cx.center}>аксессуары</Button>
-      <Button className={cx.center}>ювелирка</Button>
-      <Button className={cx.right}>Upcycle</Button>
-      <Button className={cx.right}>вторые ручки</Button>
+      <Button className={cx.right}>верхняя одежда</Button>
+      <Button className={cx.right}>обувь</Button>
+      <Button className={cx.left}>сумки</Button>
+      <Button className={cx.left}>рубашки</Button>
+      <Button className={cx.center}>платья</Button>
+      <Button className={cx.center}>костюмы</Button>
+      <Button className={cx.right}>штаны</Button>
+      <Button className={cx.right}>аксессуары</Button>
     </div>
   );
 };

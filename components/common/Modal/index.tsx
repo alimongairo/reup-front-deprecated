edf735b1ce9190b16bb55a3414c71f8ff0e4f91b
Roman <fhdha@yandex.ru>
2023-06-17 14:47:15 +0300

    finish tovar mobile

--- components/common/Modal/index.tsx
+++ components/common/Modal/index.tsx
@@ -1,10 +1,12 @@
 import { ReactNode, useEffect } from 'react';
+import { useWindowSize } from '@/hooks/useWindow';
 import classNames from 'classnames';
 import Image from 'next/image';
 
 import Portal from '@/hoc/Portal';
 
 import close from 'static/icons/close.svg';
+import leftArrowMobile from 'static/icons/ArrowLeftMobile.svg';
 
 import cx from './index.module.scss';
 
@@ -15,20 +17,30 @@ interface IProps {
 }
 
 const Modal = ({ visible, onClose, children }: IProps) => {
+  const windowSize = useWindowSize();
+
+  if (windowSize.width == undefined) {
+    windowSize.width = 390;
+  }
+
   useEffect(() => {
     if (visible) {
       document.body.style.overflow = 'hidden';
       return;
     }
     document.body.style.overflow = 'auto';
-  }, [visible]);
+  }, [visible, windowSize.width]);
 
   return (
     <Portal visible={visible}>
       <div className={cx.backModal}>
         <div className={classNames(cx.wrapper)}>
           <span className={cx.closeBtn} onClick={onClose}>
-            <Image src={close} alt={'close'} />
+            {windowSize.width <= 586 ? (
+              <Image src={leftArrowMobile} alt={'leftArrowMobile'} />
+            ) : (
+              <Image src={close} alt={'close'} />
+            )}
           </span>
           <div className={cx.content}>{children}</div>
         </div>

6cb30a398c4ade20d109245ac9c1f15482179c01
dashaBobrovad <dbobrova.dasha@gmail.com>
2023-07-24 19:38:53 +0300

    cascade works correctly

--- components/common/CollapseCascade/index.tsx
+++ components/common/CollapseCascade/index.tsx
@@ -8,11 +8,20 @@ import Image from 'next/image';
 import classNames from 'classnames';
 
 // TODO: rewrite any
-const Item = ({ children, title2, myRef, onClick }: any) => {
+const Item = ({
+  children,
+  title2,
+  myRef,
+  onClick,
+  isCloseMain,
+  setMainHeightContent,
+}: any) => {
   const [heightContent, setHeightContent] = useState(0);
   const [isClose, setIsClose] = useState(true);
 
   const toggleOpen = (e: any) => {
+    // console.log("toggleOpen");
+    // console.log(myRef)
     if (myRef && myRef.current) {
       e.stopPropagation();
       console.log('TOGGLE OPEN');
@@ -21,10 +30,14 @@ const Item = ({ children, title2, myRef, onClick }: any) => {
   };
 
   useEffect(() => {
+    console.log(isCloseMain);
+
     if (myRef && myRef.current) {
+      console.log(myRef.current);
       if (isClose) {
-        console.log(myRef.current.children[0].getBoundingClientRect().height);
-        const height = myRef.current.children[0].getBoundingClientRect().height;
+        // console.log(myRef.current.getBoundingClientRect().height);
+        const height =
+          myRef.current.children[0]?.getBoundingClientRect().height;
         const minHeight = '0px';
         if (height !== 0) {
           setHeightContent(height);
@@ -33,11 +46,20 @@ const Item = ({ children, title2, myRef, onClick }: any) => {
         return;
       }
       myRef.current.style.height = `${heightContent}px`;
+      console.log('SET MAIN');
+      setMainHeightContent((prev: number) => prev + heightContent);
     }
-  }, [isClose, myRef]);
+  }, [isClose, myRef, isCloseMain, heightContent, setMainHeightContent]);
 
+  const fn = (e: any) => {
+    if (onClick !== undefined) {
+      onClick();
+    } else {
+      toggleOpen(e);
+    }
+  };
   return (
-    <div className={cx.wrapper} onClick={toggleOpen || onClick}>
+    <div className={cx.wrapper} onClick={fn}>
       <div className={cx.titleWrapper}>
         <Heading tag="h4" className={cx.title}>
           {title2}
@@ -76,11 +98,12 @@ const CollapseCascade = (props: any) => {
     );
   }, [arrLength]);
 
-  const contentRef = useRef<HTMLDivElement>(null);
+  const contentRef = createRef<HTMLDivElement>();
   const [isCloseMain, setIsCloseMain] = useState(true);
-  const [heightContent, setHeightContent] = useState(0);
+  const [mainHeightContent, setMainHeightContent] = useState(0);
 
   const toggleOpen = () => {
+    console.log('ON CLICK');
     setIsCloseMain((state) => !state);
   };
 
@@ -91,45 +114,56 @@ const CollapseCascade = (props: any) => {
         const height = contentRef.current.getBoundingClientRect().height;
         const minHeight = '0px';
         if (height !== 0) {
-          setHeightContent(height);
+          setMainHeightContent(height);
         }
         contentRef.current.style.height = minHeight;
         return;
       }
-      contentRef.current.style.height = `${heightContent}px`;
+      contentRef.current.style.height = `${mainHeightContent}px`;
     }
-  }, [isCloseMain]);
+  }, [isCloseMain, mainHeightContent]);
 
   return (
     <Item
       key={`${content.label}-main`}
       title2={content.label}
       onClick={toggleOpen}
+      // ref={contentRef}
+      // onClick={toggleOpen}
+      // myRef={contentRef}
     >
-      <div ref={contentRef}>
+      <div ref={contentRef} className={cx.contentMain}>
+        {/* <div>dsdsdsdsd</div> */}
+        {/* <div ref={contentRef}> */}
         {
           // !content.isClose &&
+
           content.list.map((item2: any, i: number) => (
             <Item
               key={`${content.label}-inner`}
               title2={`sub: ${item2.label}`}
               myRef={elRefs[i]}
+              isCloseMain={isCloseMain}
+              setMainHeightContent={setMainHeightContent}
             >
               {/* {
               !item2.isClose && ( */}
-              <CheckboxGroup
-                key={`${content.label}`}
-                checkboxList={JSON.parse(JSON.stringify(item2.list))}
-                groupName={content.value}
-                subGroupName={item2.value}
-                direction={'vertical'}
-                onChangeGroup={props.onChange}
-              />
+              {!isCloseMain && (
+                <CheckboxGroup
+                  key={`${content.label}`}
+                  checkboxList={JSON.parse(JSON.stringify(item2.list))}
+                  groupName={content.value}
+                  subGroupName={item2.value}
+                  direction={'vertical'}
+                  onChangeGroup={props.onChange}
+                />
+              )}
               {/* )
             } */}
             </Item>
           ))
         }{' '}
+        {/* </div> */}
       </div>
     </Item>
   );

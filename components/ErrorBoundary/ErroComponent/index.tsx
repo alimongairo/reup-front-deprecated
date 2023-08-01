e447a9ff21603452b6aa2cc86cf10ca040ee452f
dashaBobrovad <dbobrova.dasha@gmail.com>
2023-08-01 19:18:40 +0300

    f

--- components/ErrorBoundary/ErroComponent/index.tsx
+++
@@ -1,5 +0,0 @@
-const ErrorComponent = () => {
-  return <h1>Что то пошло не так...</h1>;
-};
-
-export default ErrorComponent;
---
+++ pages/brand/change/index.tsx
@@ -0,0 +1,8 @@
+import MainLayout from '@/layouts/MainLayout';
+import cx from './index.module.scss';
+
+const BrandChangePage = () => {
+  return <MainLayout>BrandChangePage</MainLayout>;
+};
+
+export default BrandChangePage;

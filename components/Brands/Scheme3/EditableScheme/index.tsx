aaf9f260c042ffb876c863574173ac0330fa0e4f
dashaBobrovad <dbobrova.dasha@gmail.com>
2023-08-03 19:01:34 +0300

    banner

--- components/Brands/Scheme3/EditableScheme/index.tsx
+++ components/Brands/Scheme3/EditableScheme/index.tsx
@@ -5,29 +5,26 @@ import HeaderForEdit from '@/components/Brands/HeaderForEdit';
 import Heading from '@/components/common/Heading';
 import Text from '@/components/common/Text';
 import MoreButton from '@/components/common/MoreButton';
-
+import FirstPage from '@/components/Brands/Scheme3/FirstPage';
 import cx from './index.module.scss';
 import cxM from '@/components/Brands/Scheme3/index.module.scss';
+
+const RigtContent = () => {
+  return (
+    <div className={cx.gallery}>
+      <div className={cx.banner}>
+        <Button transparent className={cxM.uploadBtn}>
+          добавить фото / видео
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 const EditableScheme = () => {
   return (
     <>
-      <HeaderForEdit />{' '}
-      <div className={cx.gallery}>
-        <div className={cx.galleryItem}>
-          <Button transparent>добавить фото</Button>
-        </div>
-        <div className={cx.galleryItem}>
-          <Button transparent>добавить фото</Button>
-        </div>
-        <div className={cx.galleryItem}>
-          <Button transparent>добавить фото / видео</Button>
-        </div>
-        <div className={classNames(cx.galleryItem, cx.text)}>
-          <Text size="big">заголовок</Text>
-          <Text size="thin">текстовый блок с описанием товара / реклама</Text>
-        </div>
-      </div>
+      <HeaderForEdit /> <FirstPage rightContent={<RigtContent />} />
       <div className={cxM.createRow}>
         <Heading tag="h2" className={cxM.title}>
           создай свой уникальный образ вместе с Balenciaga
--- components/Brands/Scheme3/FirstPage/index.tsx
+++ components/Brands/Scheme3/FirstPage/index.tsx
@@ -5,10 +5,10 @@ import MoreButton from '@/components/common/MoreButton';
 import Heading from '@/components/common/Heading';
 
 import cx from './index.module.scss';
-
 import img from './img.png';
+import { ReactNode } from 'react';
 
-const FirstPage = () => {
+const FirstPage = ({ rightContent }: { rightContent?: ReactNode }) => {
   return (
     <div className={cx.wrapper}>
       <div className={cx.leftSide}>
@@ -16,17 +16,23 @@ const FirstPage = () => {
         <Text className={cx.aboutText}>Текст описание от бренда</Text>
       </div>
       <div className={cx.rightSide}>
-        <Image src={img} alt={'promo'} />
-        <div className={cx.nav}>
-          <div className={cx.navItems}>
-            <Text>текст</Text>
-            <Text>текст</Text>
-            <Text>текст</Text>
-            <Text>текст</Text>
-            <Text>текст</Text>
-          </div>
-          <MoreButton />
-        </div>
+        {rightContent ? (
+          rightContent
+        ) : (
+          <>
+            <Image src={img} alt={'promo'} />
+            <div className={cx.nav}>
+              <div className={cx.navItems}>
+                <Text>текст</Text>
+                <Text>текст</Text>
+                <Text>текст</Text>
+                <Text>текст</Text>
+                <Text>текст</Text>
+              </div>
+              <MoreButton />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
